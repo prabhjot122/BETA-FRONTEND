1d130194577e0962b9e892e3d3efd91fb460486c
prabhjot122 <128220378+prabhjot122@users.noreply.github.com>
2025-08-25 09:35:00 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,6 +26,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
+        <GoogleAnalytics gaId="G-SD4QKD1YNG" />
       </body>
     </html>
   );
